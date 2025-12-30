098cc9639406f2697888e9d797ab0af8f76a9d1e
AhmadEleiwa <ahmadilawa@gmail.com>
2025-12-30 20:13:05 +0200

    Update HotelCard component test cases, adding smoking test

--- src/features/hotels/HotelCard/HotelCard.test.tsx
+++ src/features/hotels/HotelCard/HotelCard.test.tsx
@@ -4,57 +4,94 @@ import HotelCard from "./HotelCard";
 
 import "@testing-library/jest-dom";
 
-import userEvent from '@testing-library/user-event';
+import userEvent from "@testing-library/user-event";
 import { Routes, Route } from "react-router-dom";
 import { render } from "../../../test/render";
 import { MOCK_HOTELS } from "../../../constants";
+// describe('components/HotelCard', () => {
+//   describe('Smoke Tests', () => {});
 
-describe("HotelCard", () => {
-  it("check default variant", () => {
-    render(<HotelCard hotel={MOCK_HOTELS[0]} />);
-    const compactElement = screen.getByTestId("hotel--compact-ui");
-    expect(compactElement).toBeInTheDocument();
+//   describe('Variants Rendering', () => {});
+
+//   describe('Conditional Rendering', () => {});
+
+//   describe('Basic Functionality', () => {});
+// });
+
+const getters = {
+  compactCard: () => screen.getByTestId("hotel--compact-ui"),
+  featuredCard: () => screen.getByTestId("hotel--featured-ui"),
+  listCard: () => screen.getByTestId("hotel--list-ui"),
+  saleBadge: () => screen.queryByTestId("hotel--featured-ui--salebadge"),
+};
+describe("features/hotels/HotelCard", () => {
+  describe("Smoke Tests", () => {
+    it("renders compact variant without crashing", () => {
+      render(<HotelCard hotel={MOCK_HOTELS[0]} variant="compact" />);
+    });
+
+    it("renders featured variant without crashing", () => {
+      render(<HotelCard hotel={MOCK_HOTELS[0]} variant="featured" />);
+    });
+
+    it("renders list variant without crashing", () => {
+      render(<HotelCard hotel={MOCK_HOTELS[0]} variant="list" />);
+    });
   });
-  it("renders compact variant when variant='compact'", () => {
-    render(<HotelCard hotel={MOCK_HOTELS[0]} variant="compact" />);
-    const compactElement = screen.getByTestId("hotel--compact-ui");
-    expect(compactElement).toBeInTheDocument();
+
+  describe("Variants Rendering", () => {
+    it("check default variant", () => {
+      render(<HotelCard hotel={MOCK_HOTELS[0]} />);
+      const compactElement = getters.compactCard()
+      expect(compactElement).toBeInTheDocument();
+    });
+    it("renders compact variant when variant='compact'", () => {
+      render(<HotelCard hotel={MOCK_HOTELS[0]} variant="compact" />);
+      const compactElement = getters.compactCard()
+      expect(compactElement).toBeInTheDocument();
+    });
+    it("renders compact variant when variant='featured'", () => {
+      render(<HotelCard hotel={MOCK_HOTELS[0]} variant="featured" />);
+      const featuredElement = getters.featuredCard()
+      expect(featuredElement).toBeInTheDocument();
+    });
+    it("renders compact variant when variant='list'", () => {
+      render(<HotelCard hotel={MOCK_HOTELS[0]} variant="list" />);
+      const listElement = getters.listCard()
+      expect(listElement).toBeInTheDocument();
+    });
   });
-  it("renders compact variant when variant='featured'", () => {
-    render(<HotelCard hotel={MOCK_HOTELS[0]} variant="featured" />);
-    const compactElement = screen.getByTestId("hotel--featured-ui");
-    expect(compactElement).toBeInTheDocument();
-  });
-  it("renders compact variant when variant='list'", () => {
-    render(<HotelCard hotel={MOCK_HOTELS[0]} variant="list" />);
-    const compactElement = screen.getByTestId("hotel--list-ui");
-    expect(compactElement).toBeInTheDocument();
-  });
+  describe("Conditional Rendering", () => {
     it("renders compact variant when variant='featured' and showSaleBadge=false ", () => {
-    render(<HotelCard hotel={MOCK_HOTELS[0]} variant="featured" />);
-    const compactElement = screen.queryByTestId("hotel--featured-ui--salebadge");
-    expect(compactElement).not.toBeInTheDocument()
+      render(<HotelCard hotel={MOCK_HOTELS[0]} variant="featured" />);
+      const compactElement = getters.saleBadge()
+      expect(compactElement).not.toBeInTheDocument();
+    });
+    it("renders compact variant when variant='featured' and showSaleBadge=true ", () => {
+      render(
+        <HotelCard hotel={MOCK_HOTELS[0]} showSaleBadge variant="featured" />
+      );
+      const compactElement = getters.saleBadge()
+      expect(compactElement).toBeInTheDocument();
+    });
   });
-      it("renders compact variant when variant='featured' and showSaleBadge=true ", () => {
-    render(<HotelCard hotel={MOCK_HOTELS[0]} showSaleBadge variant="featured" />);
-    const compactElement = screen.queryByTestId("hotel--featured-ui--salebadge");
-    expect(compactElement).toBeInTheDocument()
-  });
-it('navigates to correct hotel url', () => {
-  render(<HotelCard hotel={MOCK_HOTELS[0]} />);
+  describe("Basic Functionality", () => {
+    it("navigates to correct hotel url", () => {
+      render(<HotelCard hotel={MOCK_HOTELS[0]} />);
 
-  const link = screen.getByTestId('hotel--compact-ui');
-  expect(link).toHaveAttribute('href', `/hotel/${MOCK_HOTELS[0].id}`);
-});
-it('navigates to hotel details page when clicked', async () => {
-  render(
-    <Routes>
-      <Route path="/" element={<HotelCard hotel={MOCK_HOTELS[0]} />} />
-      <Route path="/hotel/:id" element={<h1>Hotel Details</h1>} />
-    </Routes>,
-  );
-  const link = screen.getByTestId('hotel--compact-ui');
-  await userEvent.click(link)
-  expect(screen.getByText('Hotel Details')).toBeInTheDocument();
-});
+      const link = screen.getByTestId("hotel--compact-ui");
+      expect(link).toHaveAttribute("href", `/hotel/${MOCK_HOTELS[0].id}`);
+    });
+    it("navigates to hotel details page when clicked", async () => {
+      render(
+        <Routes>
+          <Route path="/" element={<HotelCard hotel={MOCK_HOTELS[0]} />} />
+          <Route path="/hotel/:id" element={<h1>Hotel Details</h1>} />
+        </Routes>
+      );
+      const link = screen.getByTestId("hotel--compact-ui");
+      await userEvent.click(link);
+      expect(screen.getByText("Hotel Details")).toBeInTheDocument();
+    });
+  });
 });
