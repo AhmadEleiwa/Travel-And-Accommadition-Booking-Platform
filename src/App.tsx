2bfbf9648614e285dbbc02597095435fa424d324
AhmadEleiwa <ahmadilawa@gmail.com>
2025-12-30 17:50:35 +0200

    Add MUI library to project dependincies

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,31 @@
-import { HashRouter } from 'react-router-dom';
+// import { HashRouter, Routes, Route } from 'react-router-dom'; // 1. Import Routes
+// import HotelCard from './features/hotels/HotelCard';
+// import { MOCK_HOTELS } from './constants';
 
-
+import { Container } from "@mui/material";
+import { MOCK_HOTELS } from "./constants";
+import HotelCard from "./features/hotels/HotelCard";
+import { HashRouter, Route, Routes } from "react-router-dom";
 
 export default function App() {
   return (
+    // <h1>sdsd</h1>
     <HashRouter>
-      <div className="min-h-screen flex flex-col">
-        {/* here the navbar */}
-        <main className="flex-grow">
-      {/* here the routes */}
-        </main>
-
-      </div>
+      <Routes>
+        {" "}
+        {/* 2. Add this wrapper */}
+        <Route
+          index
+          path="/"
+          element={
+            <div>
+              <Container sx={{ width: "100%", margin: "10%" }}>
+                <HotelCard variant="featured" hotel={MOCK_HOTELS[0]} />
+              </Container>
+            </div>
+          }
+        />
+      </Routes>
     </HashRouter>
   );
 }
