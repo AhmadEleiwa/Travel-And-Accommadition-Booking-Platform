bdc608e67717cdce8b3d681c03bc253324c62baf
AhmadEleiwa <ahmadilawa@gmail.com>
2025-12-30 13:23:17 +0200

    Fix test errors for HotelCard

--- src/components/HotelCard/HotelCard.test.tsx
+++ src/components/HotelCard/HotelCard.test.tsx
@@ -1,5 +1,4 @@
-import { describe } from "node:test";
-import { expect } from "vitest";
+import { describe, it, expect } from "vitest";
 import { screen } from "@testing-library/react";
 import HotelCard from "./HotelCard";
 
