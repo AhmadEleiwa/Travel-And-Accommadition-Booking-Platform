e8646ca8766a8a23a84cee46c96db9fac7fc9173
AhmadEleiwa <ahmadilawa@gmail.com>
2025-12-31 22:36:03 +0200

    Update App and adding Navbar and handling Mock user state

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,19 @@
 // import { HashRouter, Routes, Route } from 'react-router-dom'; // 1. Import Routes
 // import HotelCard from './features/hotels/HotelCard';
 // import { MOCK_HOTELS } from './constants';
-
-import { Container } from "@mui/material";
-import { MOCK_HOTELS } from "./constants";
-import HotelCard from "./features/hotels/HotelCard";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter} from "react-router-dom";
+import { AppRouter } from "./routes/router";
+import Navbar from "./layouts/Navbar/Navbar";
+import type { User } from "./features/auth";
+import { useState } from "react";
 
 export default function App() {
+  const [mockUser,setMockUser] = useState<User | null>({email:'ahmad@gmail.com', role:'USER', username:"ss", id:'s' })
   return (
     // <h1>sdsd</h1>
-    <HashRouter>
-      <Routes>
-        {" "}
-        {/* 2. Add this wrapper */}
-        <Route
-          index
-          path="/"
-          element={
-            <div>
-              <Container sx={{ width: "100%", margin: "10%" }}>
-                <HotelCard variant="featured" hotel={MOCK_HOTELS[0]} />
-              </Container>
-            </div>
-          }
-        />
-      </Routes>
-    </HashRouter>
+    <BrowserRouter>
+      <Navbar user={mockUser} onLogout={()=>{setMockUser(null)}} />
+      <AppRouter />
+    </BrowserRouter>
   );
 }
