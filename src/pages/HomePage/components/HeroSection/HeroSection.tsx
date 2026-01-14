d3d916fed63d94c457e14dd6dd67473d2b3ce9f3
AhmadEleiwa <ahmadilawa@gmail.com>
2026-01-14 21:07:49 +0200

    refactor: move the SearchBar into component/ folder

--- src/pages/HomePage/components/HeroSection/HeroSection.tsx
+++ src/pages/HomePage/components/HeroSection/HeroSection.tsx
@@ -1,5 +1,5 @@
 import { Box, Typography } from "@mui/material";
-import SearchBar from "../../../../features/search/SearchBar";
+import SearchBar from "../../../../features/search/components/SearchBar";
 import type React from "react";
 
 export const HeroSection: React.FC = () => {
